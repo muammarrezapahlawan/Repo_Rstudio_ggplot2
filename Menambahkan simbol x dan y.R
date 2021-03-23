64ac5574cb389c61cc7af22bd727022f256ab7f6
M Reza Pahlawan <macbookpro@MacBook-Pro-Macbook.local>
2021-03-23 18:56:47 +0800

    Menambahkan Sumbu x dan y pada kanvas

---
+++ Menambahkan simbol x dan y.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(ggplot2)
+
+plot.jakarta <- ggplot()
+plot.jakarta <- plot.jakarta + labs + (title="Luas wilayah Vs Kepadatan penduduk jakarta - periode 2013")
+plot.jakarta <- plot.jakarta + labs + (subtitle="Tahun 2013")
+plot.jakarta <- plot.jakarta + labs(x="Luas wilayah (KM2)",y="Kepadatan Pendudukd")
+plot.jakarta
+
+
