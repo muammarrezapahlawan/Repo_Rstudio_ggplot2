b116e8b1f00d2abed0d63b27b0ddf8ddeec4191a
M Reza Pahlawan <macbookpro@MacBook-Pro-Macbook.local>
2021-03-26 12:39:37 +0800

    Scater_plot_kepadatan_pend_jkt_dgn_func_layer

---
+++ Scatter plot Kepadatan Penduduk Jakarta dengan function layer.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(ggplot2)
+#Membaca data csv dan dimasukkan ke variable penduduk.dki
+#penduduk.dki <- read.csv("/Users/macbookpro/Dev/tutorial r/DQLab/data-kepadatan-dan-luas-wilayah-per-kelurahan-di-provinsi-dki-jakarta-tahun-2013.csv", sep=",")
+penduduk.dki <- read.csv("https://storage.googleapis.com/dqlab-dataset/dkikepadatankelurahan2013.csv", sep=",")
+plot.dki <- ggplot(data=penduduk.dki, aes(x = LUAS.WILAYAH..KM2., y=KEPADATAN..JIWA.KM2.,  color=NAMA.KABUPATEN.KOTA))
+plot.dki + layer(geom ="point", stat="identity", position = "identity")
+
+
